aa9edb9b59ec2abe5fd0743d4458e8be7b6cf09f
Aditya Gupta <adityagupta1620@gmail.com>
2025-04-20 12:32:49 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Button variant="ghost" className="bg-red-500" >Click me</Button>
+
+    </div>
+  )
+}
+
+export default App
